7a32dbb1d8ba0408cf2daca653736be5cd68748b
MartinVillatoros <martinguille17@gmail.com>
2025-03-27 19:09:57 -0600

    Pruebas JUnit

---
+++ BinaryTreeTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinaryTreeTest {
+
+    private BinaryTree<String, String> arbol;
+
+    @BeforeEach
+    public void setUp() {
+        arbol = new BinaryTree<>();
+    }
+
+    @Test
+    public void testInsertarYBuscar() {
+        arbol.insertar("001", "Zapatos deportivos");
+        arbol.insertar("002", "Camiseta técnica");
+
+        assertEquals("Zapatos deportivos", arbol.buscar("001"));
+        assertEquals("Camiseta técnica", arbol.buscar("002"));
+    }
+
+   
+}
